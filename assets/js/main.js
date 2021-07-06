d2953096c7bae1b136ff32de3e38c6836d8e6963
Belal Khan <belalkhan.dev@gmail.com>
2021-07-06 15:19:03 +0600

    design search box

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,31 @@
     "use-strict"
 
     jQuery(document).ready(function(){
+        $('.search-box-body').css({
+            "min-height": document.querySelector(".search-box-items.active").clientHeight
+        });
+
+        window.addEventListener('resize', function(event) {
+            $('.search-box-body').css({
+                "min-height": document.querySelector(".search-box-items.active").clientHeight
+            });
+    
+        }, true);
+
+        
+        //on click tab item
+        $(document).on('click', '.search-item', function() {
+            let _self = $(this);
+            let target_item = _self.data('target_item');          
+            $('.search-item, .search-box-items').removeClass('active');
+            $(_self).addClass('active');
+            $(target_item).addClass('active');
+            let box_height = document.querySelector(target_item).clientHeight;
+            console.log(box_height);
+            $('.search-box-body').css({
+                "min-height": box_height
+            })
+        })
 
         //sliders 
         if($('.sliders').length > 0) {
@@ -19,6 +44,7 @@
                 dots: false
             });
         }
+
 
     });
 
